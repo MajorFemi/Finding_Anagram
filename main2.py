42d396a4f4044c6b4a54234dd58bfffa0842746c
Alao Oluwafemi <55229710+MajorFemi@users.noreply.github.com>
2022-05-25 19:24:53 +0100

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,32 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+from collections import Counter
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    if(Counter(word) != Counter(anagram)):
+        print (False)
+    else:
+        return True
+
+while True:
+    word = (input("Enter the first word: ").lower())
+    if not word.isalpha():
+        print("Please enter only alphabetical words: ")
+        continue
+    else:
+        break
+
+while True:
+    anagram = (input("Enter the second word: ").lower())
+    if not anagram.isalpha():
+        print("Please enter only alphabetical words: ")
+        continue
+    else:
+        break
+            
+find_anagram(word, anagram)
+
